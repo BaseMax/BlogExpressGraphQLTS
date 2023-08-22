be0fde20a08edb0ce5f32b2779b3681072a28c67
khalvai <khalvayozbek@gmail.com>
2023-08-22 09:55:39 +0330

    remove: extra middleware

--- src/helper/auth-checker.ts
+++
@@ -1,8 +0,0 @@
-import { AuthPayload } from "../auth/entity/auth-payload";
-
-export const authChecker = () => (user: AuthPayload | undefined) => {
-  if (!user) {
-    return false;
-  }
-  return true;
-};
